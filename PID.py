be86412b4af5d230431f23456aab7703ac202515
Jacob Epstein <jacobepstein90@gmail.com>
2022-09-06 01:22:11 -0400

    pygame hello world

---
+++ PID.py
@@ -0,0 +1,31 @@
+import pygame, sys
+
+def main():
+    pygame.init()
+
+    size = width, height = 600, 450
+    speed = [2, 2]
+    black = 0, 0, 0
+
+    screen = pygame.display.set_mode(size)
+
+    ball = pygame.image.load("ball.jpeg")
+    ball = pygame.transform.scale(ball, (20, 20))
+    ballrect = ball.get_rect()
+
+    while 1:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+
+        ballrect = ballrect.move(speed)
+        if ballrect.left < 0 or ballrect.right > width:
+            speed[0] = -speed[0]
+        if ballrect.top < 0 or ballrect.bottom > height:
+            speed[1] = -speed[1]
+
+        screen.fill(black)
+        screen.blit(ball, ballrect)
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
